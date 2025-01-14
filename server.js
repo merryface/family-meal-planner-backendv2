66d84a2fb3cb6862f6363cd758c9b1ffcf783ce3
Tarik Merrylees <tarikmerrylees@gmail.com>
2025-01-14 11:58:21 +0000

    add a test endpoint

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/test', (req, res) => {
+  res.json({ message: 'API is running!' });
+});
